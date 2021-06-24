68875394d7285e37712ca8f4be2fd66934465da5
shady samara <shadysamara@shadys-MacBook-Pro.local>
2021-06-24 11:51:37 +0300

    assigment

--- lib/services/sharedprefrences_helper.dart
+++ lib/services/sharedprefrences_helper.dart
@@ -1,5 +1,12 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// create register and login form and make validation
+/// 1- email validation
+/// 2- required validation
+/// 3- password must be larger than 6 letters
+/// when the user register in the app, his data will be saved in shared prefrences
+/// when the user open the app it will redirect him to login or home page
+/// based on isLogged key in the sharedprefrences
 class SpHelper {
   static final SpHelper spHelper = SpHelper();
   SharedPreferences sharedPreferences;
